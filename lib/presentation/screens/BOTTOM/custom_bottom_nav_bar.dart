110c6cdcbe4df87fc25b98339970072b6bb0dd6f
Leeerrrmmm <leeerrrmmm.@MacBook-Air-Leeerrrmmm.local>
2025-04-02 20:38:53 +0300

    adapt devices

--- lib/presentation/screens/BOTTOM/custom_bottom_nav_bar.dart
+++ lib/presentation/screens/BOTTOM/custom_bottom_nav_bar.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mind_horizon/presentation/screens/MAIN/main_screen.dart';
