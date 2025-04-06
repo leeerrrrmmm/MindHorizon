ab65a7dc0bbb2120821c16e390661e6f134dd39d
Leeerrrmmm <leeerrrmmm.@MacBook-Air-Leeerrrmmm.local>
2025-04-06 11:27:38 +0300

    add music

--- lib/data/data_source/data_source.dart
+++ lib/data/data_source/data_source.dart
@@ -248,12 +248,11 @@ final List<CategoryModel> sleepCategory = [
         imagePath: 'assets/img/bat.png',
         largestImagepath: 'assets/img/lbat.png',
         sounds: [
-          SoundsModel(id: 0, title: 'Sleep cpace', musicAsset: ''),
-          SoundsModel(id: 1, title: 'Time for sweet dreams', musicAsset: ''),
-          SoundsModel(id: 2, title: 'Melody for the soul', musicAsset: ''),
-          SoundsModel(id: 3, title: 'Tranquil sleep', musicAsset: ''),
-          SoundsModel(id: 4, title: 'Magical garden', musicAsset: ''),
-          SoundsModel(id: 5, title: 'Gentle flute', musicAsset: ''),
+          SoundsModel(
+            id: 0,
+            title: 'Sleep cpace',
+            musicAsset: 'sound/lslep.mp3',
+          ),
         ],
         colors: [
           // fCOlor
@@ -289,16 +288,7 @@ final List<CategoryModel> sleepCategory = [
         imagePath: 'assets/img/water.png',
         largestImagepath: 'assets/img/lwawe.png',
         sounds: [
-          SoundsModel(id: 0, title: 'Nirvana', musicAsset: ''),
-          SoundsModel(id: 1, title: 'Serenity', musicAsset: ''),
-          SoundsModel(id: 2, title: 'Peace of mind', musicAsset: ''),
-          SoundsModel(id: 3, title: 'Enlightment', musicAsset: ''),
-          SoundsModel(id: 4, title: 'Drowsiness', musicAsset: ''),
-          SoundsModel(
-            id: 5,
-            title: 'The world of dreams and fantasies',
-            musicAsset: '',
-          ),
+          SoundsModel(id: 0, title: 'Nirvana', musicAsset: 'sound/lwave.mp3'),
         ],
         colors: [
           // SoundsModelfCOlor
@@ -334,12 +324,7 @@ final List<CategoryModel> sleepCategory = [
         imagePath: 'assets/img/amico.png',
         largestImagepath: 'assets/img/lnature.png',
         sounds: [
-          SoundsModel(id: 0, title: 'Ocean breeze', musicAsset: ''),
-          SoundsModel(id: 1, title: 'By the fire', musicAsset: ''),
-          SoundsModel(id: 2, title: 'Fresh wind', musicAsset: ''),
-          SoundsModel(id: 3, title: 'Waterfall', musicAsset: ''),
-          SoundsModel(id: 4, title: 'The sound of rain', musicAsset: ''),
-          SoundsModel(id: 5, title: 'Rain with thunderstorm', musicAsset: ''),
+          SoundsModel(id: 0, title: 'Nature', musicAsset: 'sound/lnature.mp3'),
         ],
         colors: [
           // fCOlor
@@ -468,14 +453,18 @@ final List<CategoryModel> meditationCategory = [
         description: 'Relax and fall asleep',
         imagePath: 'assets/img/sleep.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 6, title: 'Step 7', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 7, title: 'Step 8', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/slep.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/sleep.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/sleeep.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/sleeeep.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/sleeeeep.mp3'),
+          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/sleeeeeep.mp3'),
+          StepsModel(id: 6, title: 'Step 7', stepAsset: 'sound/sleeeeeeep.mp3'),
+          StepsModel(
+            id: 7,
+            title: 'Step 8',
+            stepAsset: 'sound/sleeeeeeeep.mp3',
+          ),
         ],
         colors: [
           // fCOlor
@@ -510,11 +499,11 @@ final List<CategoryModel> meditationCategory = [
         description: 'Fall asleep faster and easier',
         imagePath: 'assets/img/sleepp.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/slepp.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/sleppp.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/slepppp.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/sleppppp.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/slepppppp.mp3'),
         ],
         colors: [
           // fCOlor
@@ -554,14 +543,18 @@ final List<CategoryModel> meditationCategory = [
         description: 'Unleash your individuality',
         imagePath: 'assets/img/love.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 6, title: 'Step 7', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 7, title: 'Step 8', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/love.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/lovee.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/loveee.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/loveeee.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/loveeeee.mp3'),
+          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/loveeeeee.mp3'),
+          StepsModel(id: 6, title: 'Step 7', stepAsset: 'sound/loveeeeeee.mp3'),
+          StepsModel(
+            id: 7,
+            title: 'Step 8',
+            stepAsset: 'sound/loveeeeeeee.mp3',
+          ),
         ],
         colors: [
           // fCOlor
@@ -596,14 +589,14 @@ final List<CategoryModel> meditationCategory = [
         description: 'Get your body in shape',
         imagePath: 'assets/img/shape.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 6, title: 'Step 7', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 7, title: 'Step 8', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/los.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/loss.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/losss.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/lossss.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/losssss.mp3'),
+          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/lossssss.mp3'),
+          StepsModel(id: 6, title: 'Step 7', stepAsset: 'sound/losssssss.mp3'),
+          StepsModel(id: 7, title: 'Step 8', stepAsset: 'sound/lossssssss.mp3'),
         ],
         colors: [
           // fCOlor
@@ -643,10 +636,10 @@ final List<CategoryModel> meditationCategory = [
         description: 'Become more successful and productive',
         imagePath: 'assets/img/prod.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/clap.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/clap.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/prod.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/prodd.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/proddd.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/prodddd.mp3'),
         ],
         colors: [
           // fCOlor
@@ -681,10 +674,14 @@ final List<CategoryModel> meditationCategory = [
         description: 'Complete tasks quckly and efficientle',
         imagePath: 'assets/img/procr.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/procrast.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/procrastt.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/procrasttt.mp3'),
+          StepsModel(
+            id: 3,
+            title: 'Step 4',
+            stepAsset: 'sound/procrastttt.mp3',
+          ),
         ],
         colors: [
           // fCOlor
@@ -724,12 +721,20 @@ final List<CategoryModel> meditationCategory = [
         description: 'Get rid of fear and worries',
         imagePath: 'assets/img/anxi.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/anxiety.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/anxietyy.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/anxietyyy.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/anxietyyyy.mp3'),
+          StepsModel(
+            id: 4,
+            title: 'Step 5',
+            stepAsset: 'sound/anxietyyyyy.mp3',
+          ),
+          StepsModel(
+            id: 5,
+            title: 'Step 6',
+            stepAsset: 'sound/anxietyyyyyy.mp3',
+          ),
         ],
         colors: [
           // fCOlor
@@ -764,11 +769,11 @@ final List<CategoryModel> meditationCategory = [
         description: 'Release tension and stress',
         imagePath: 'assets/img/stress.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/stres.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/stress.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/stresss.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/stressss.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/stresssss.mp3'),
         ],
         colors: [
           // fCOlor
@@ -808,36 +813,36 @@ final List<CategoryModel> meditationCategory = [
         description: 'Learn to accept and forgive',
         imagePath: 'assets/img/sun.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/abil.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/abill.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/abilll.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/abillll.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/abilllll.mp3'),
+          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/abillllll.mp3'),
         ],
         colors: [
           // fCOlor
-          Color(0xffa3fdbd),
+          Color(0xfffff0c6),
           //sColor
-          Color(0xff92e3a9),
+          Color(0xffffd252),
           //text&btnColor
-          Color(0xff263238),
+          Color(0xffffc727),
           //backImg Color
-          Color(0xffa3fdbd),
+          Color(0xffffc727),
           //category title Color
-          Color(0xff92e3a9).withValues(alpha: 0.77),
+          Color(0xffffc727),
           //list color == 2
-          Color.fromRGBO(146, 227, 169, 0.6),
+          Colors.transparent,
           //list color != 2
-          Color(0xff92e3a9),
+          Colors.transparent,
           //backgroundColor
-          Color(0xffe3fce8),
+          Color(0xfffafce3),
           //flowerElem AT secondScreen Color
-          Color(0xffd6f4de),
+          Color(0xfff1f4d6),
           //steptTitleColor
-          Color(0xff263238).withValues(alpha: 0.6),
+          Color(0xffffc727),
           //secondScreenBackgroundColor
-          Color(0xffe3fce8),
+          Color(0xfffafce3),
           // SHADOW ON SECOND  SLEEP SCREEN
           Color(0xff314b4b).withValues(alpha: 1.8),
         ],
@@ -848,38 +853,38 @@ final List<CategoryModel> meditationCategory = [
         description: 'Rise like a phoenix',
         imagePath: 'assets/img/rise.png',
         steps: [
-          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/cosmo.mp3'),
-          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/cosmo.mp3'),
+          StepsModel(id: 0, title: 'Step 1', stepAsset: 'sound/after.mp3'),
+          StepsModel(id: 1, title: 'Step 2', stepAsset: 'sound/affter.mp3'),
+          StepsModel(id: 2, title: 'Step 3', stepAsset: 'sound/afffter.mp3'),
+          StepsModel(id: 3, title: 'Step 4', stepAsset: 'sound/affffter.mp3'),
+          StepsModel(id: 4, title: 'Step 5', stepAsset: 'sound/afffffter.mp3'),
+          StepsModel(id: 5, title: 'Step 6', stepAsset: 'sound/affffffter.mp3'),
         ],
         colors: [
           // fCOlor
-          Color(0xffa3fdbd),
+          Color(0xff84a8fa),
           //sColor
-          Color(0xff92e3a9),
-          //text&btnColor
-          Color(0xff263238),
+          Color(0xff407bff),
+          //text&btn&backImg Color
+          Color(0xff2d57b3),
           //backImg Color
-          Color(0xffa3fdbd),
+          Color(0xff2d57b3),
           //category title Color
-          Color(0xff92e3a9).withValues(alpha: 0.77),
+          Colors.transparent,
           //list color == 2
-          Color.fromRGBO(146, 227, 169, 0.6),
+          Colors.transparent,
           //list color != 2
-          Color(0xff92e3a9),
+          Colors.transparent,
           //backgroundColor
-          Color(0xffe3fce8),
+          Color(0xffe3e4fc),
           //flowerElem AT secondScreen Color
-          Color(0xffd6f4de),
+          Color(0xffd9d6f4),
           //steptTitleColor
-          Color(0xff263238).withValues(alpha: 0.6),
+          Color(0xff2d57b3),
           //secondScreenBackgroundColor
-          Color(0xffe3fce8),
+          Color(0xfff4d6d6),
           // SHADOW ON SECOND  SLEEP SCREEN
-          Color(0xff314b4b).withValues(alpha: 1.8),
+          Color(0xfff4d6d6),
         ],
       ),
     ],
@@ -896,12 +901,7 @@ final List<CategoryModel> myCourseCategory = [
         description: '',
         imagePath: 'assets/img/secanxiety.png',
         sounds: [
-          SoundsModel(id: 0, title: 'Sleep cpace', musicAsset: ''),
-          SoundsModel(id: 1, title: 'Time for sweet dreams', musicAsset: ''),
-          SoundsModel(id: 2, title: 'Melody for the soul', musicAsset: ''),
-          SoundsModel(id: 3, title: 'Tranquil sleep', musicAsset: ''),
-          SoundsModel(id: 4, title: 'Magical garden', musicAsset: ''),
-          SoundsModel(id: 5, title: 'Gentle flute', musicAsset: ''),
+          SoundsModel(id: 0, title: 'Sleep cpace', musicAsset: 'sound/anx.mp3'),
         ],
         colors: [
           // fCOlor
@@ -936,12 +936,7 @@ final List<CategoryModel> myCourseCategory = [
         description: '',
         imagePath: 'assets/img/happiness.png',
         sounds: [
-          SoundsModel(id: 0, title: 'Sleep cpace', musicAsset: ''),
-          SoundsModel(id: 1, title: 'Time for sweet dreams', musicAsset: ''),
-          SoundsModel(id: 2, title: 'Melody for the soul', musicAsset: ''),
-          SoundsModel(id: 3, title: 'Tranquil sleep', musicAsset: ''),
-          SoundsModel(id: 4, title: 'Magical garden', musicAsset: ''),
-          SoundsModel(id: 5, title: 'Gentle flute', musicAsset: ''),
+          SoundsModel(id: 0, title: 'Sleep cpace', musicAsset: 'sound/hap.mp3'),
         ],
         colors: [
           // fCOlor
@@ -976,12 +971,11 @@ final List<CategoryModel> myCourseCategory = [
         description: '',
         imagePath: 'assets/img/stres.png',
         sounds: [
-          SoundsModel(id: 0, title: 'Sleep cpace', musicAsset: ''),
-          SoundsModel(id: 1, title: 'Time for sweet dreams', musicAsset: ''),
-          SoundsModel(id: 2, title: 'Melody for the soul', musicAsset: ''),
-          SoundsModel(id: 3, title: 'Tranquil sleep', musicAsset: ''),
-          SoundsModel(id: 4, title: 'Magical garden', musicAsset: ''),
-          SoundsModel(id: 5, title: 'Gentle flute', musicAsset: ''),
+          SoundsModel(
+            id: 0,
+            title: 'Sleep cpace',
+            musicAsset: 'sound/relie.mp3',
+          ),
         ],
         colors: [
           // fCOlor
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import firebase_storage
 import google_sign_in_ios
 import path_provider_foundation
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -21,4 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
